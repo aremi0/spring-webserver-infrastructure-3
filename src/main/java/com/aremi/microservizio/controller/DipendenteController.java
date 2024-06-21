aa6ad859435dfd8e521a2c733878a61e546b661b
aremi0 <gabriele.passarello98@gmail.com>
2024-06-21 18:16:09 +0200

    inserito primo endpoint

---
+++ src/main/java/com/aremi/microservizio/controller/DipendenteController.java
@@ -0,0 +1,31 @@
+package com.aremi.microservizio.controller;
+
+import com.aremi.microservizio.dto.GenericResponse;
+import com.aremi.microservizio.dto.bean.DipendenteBean;
+import com.aremi.microservizio.service.DipendenteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/api")
+public class DipendenteController {
+    private final Logger logger;
+    private final DipendenteService dipendenteService;
+
+    @Autowired
+    public DipendenteController(DipendenteService dipendenteService) {
+        this.logger = Logger.getLogger("DipendenteController_Logger");
+        this.dipendenteService = dipendenteService;
+    }
+
+    @GetMapping("/dipendente/{id}")
+    GenericResponse<DipendenteBean> getDipendenteById(@PathVariable("id") Long id) {
+        logger.info("RestController::getDipendenteById started...");
+        return dipendenteService.getDipendenteBeanById(id);
+    }
+}
