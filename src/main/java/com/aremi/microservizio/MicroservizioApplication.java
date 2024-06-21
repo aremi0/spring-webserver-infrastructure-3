d01bef6fc795d9a656e1f8da058daa6eda1c705c
aremi0 <gabriele.passarello98@gmail.com>
2024-06-21 13:07:52 +0200

    first commit

---
+++ src/main/java/com/aremi/microservizio/MicroservizioApplication.java
@@ -0,0 +1,13 @@
+package com.aremi.microservizio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservizioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservizioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aremi/microservizio/MicroservizioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aremi.microservizio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservizioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
